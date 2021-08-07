5c2ced297cb7b0fd113271619ea215424b7fe1de
Blake Sutton <bsutton21@gmail.com>
2021-08-06 21:24:42 -0500

    commit

--- Packages.py
+++ Packages.py
@@ -5,31 +5,6 @@ from ReadCSVData import *
 
 import datetime
 
-# class Package(object):
-def __init__(self, plist):
-    self.package_id = int(plist[0])
-    self.address = plist[1]
-    self.city = plist[2]
-    self.state = plist[3]
-    self.zip = plist[4]
-    self.deadline = plist[5]
-    self.weight = plist[6]
-    self.notes = plist[7]
-    self.delivery_time = "00:00"
-    self.delivery_status = "At hub"
-
-def print_package(self):
-    print("Package ID: %i" % self.package_id)
-    print("Address:  %s" % self.address)
-    print("City: %s" % self.city)
-    print("State: %s" % self.state)
-    print("Zip: %s" % self.zip)
-    print("Deadline: %s" % self.deadline)
-    print("Weight: %s" % self.weight)
-    print("Notes: %s" % self.notes)
-    print("Delivery Time: %s" % self.delivery_time)
-    print("Status: %s" % self.delivery_status)
-
 first_delivery = []
 second_delivery = []
 third_delivery = []
